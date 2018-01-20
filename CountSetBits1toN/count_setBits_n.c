506cb62400c4c5d4dcfb7437666b13b64434335f
Ethan <haoz86@gmail.com>
2018-01-20 03:14:40 -0500

    Add some explanation

--- CountSetBits1toN/count_setBits_n.c
+++ CountSetBits1toN/count_setBits_n.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* For any 2^b(b >= 0), like 1, 2, 4, 8..., the result would be b * 2^(b-1) + 1
+** And it must be considered that result for lower bits would be affected by the number of 1 in higher bits.
+** Just count how many 1s in the integer and the index where these 1s are in the integer.
+*/
+
 int
 countSetBits( int n ) {
    int counter = 0;
